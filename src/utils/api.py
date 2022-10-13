14f94f7d7974318460abaa1d4ff2aca554189f0e
timelessnesses <mooping3roblox@gmail.com>
2022-10-13 12:54:41 +0700

    pull

--- src/utils/api.py
+++ src/utils/api.py
@@ -4,6 +4,9 @@ import enum
 import datetime
 import dataclasses
 import dateutil.parser
+import yarl
+from .db import db
+from .errors import APIError
 class Request:
     @staticmethod
     async def get(url: str, headers: dict = None):
@@ -38,6 +41,48 @@ class Badge:
     label: str
     ts: typing.Optional[datetime.datetime]
 
+class Rank(enum.Enum):
+    unranked = "z"
+    d = "d"
+    d_plus = "d+"
+    c_minus = "c-"
+    c = "c"
+    c_plus = "c+"
+    b_minus = "b-"
+    b = "b"
+    b_plus = "b+"
+    a_minus = "a-"
+    a = "a"
+    a_plus = "a+"
+    s_minus = "s-"
+    s = "s"
+    s_plus = "s+"
+    ss = "ss"
+    u = "u"
+    x = "x"
+    
+    def __str__(self) -> typing.AnyStr:
+        return self.value if self.name != "unranked" else "Unranked"
+
+@dataclasses.dataclass
+class League:
+    gamesplayed: int
+    gameswon: int
+    rating: float
+    glicko: float
+    rd: float
+    rank: Rank
+    apm: float
+    pps: float
+    vs: float
+    decaying: bool
+    standing: int
+    standing_local: int
+    prev_rank: typing.Optional[Rank]
+    prev_at: typing.Optional[int]
+    next_rank: typing.Optional[Rank]
+    next_at: typing.Optional[int]
+    percentile_rank: Rank
 @dataclasses.dataclass
 class User:
     id: str
@@ -46,6 +91,19 @@ class User:
     ts: typing.Optional[datetime.datetime]
     botmaster: typing.Optional["User"]
     badges: typing.List[Badge]
+    xp: float
+    gamesplayed: int
+    gameswon: int
+    gametime: datetime.timedelta
+    country: typing.Optional[str]
+    supporter: bool
+    supporter_tier: int
+    verified: bool
+    league: typing.Optional[League]
+    avatar_revision: typing.Optional[yarl.URL]
+    banner_revision: typing.Optional[yarl.URL]
+    bio: typing.Optional[str]
+    friend_count: int
 @dataclasses.dataclass
 class Data:
     user: typing.Optional[User]
@@ -55,6 +113,18 @@ class PlayerAPI:
     error: typing.Optional[str]
     cache: typing.Optional[Cache]
     data: typing.Optional[Data]
+    @classmethod
+    async def get_player(username:str) -> "PlayerAPI":
+        cache = await db.fetch("SELECT * FROM cache WHERE username = $1", username)
+        base_url = yarl.URL("https://ch.tetr.io/api/users")
+        if cache:
+            # filter some data that isn't exists or none
+            result:dict = cache[0]
+            if not result["success"]:
+                response = await (await Request.get(base_url/username)).json()
+                raise APIError(result["error"])
+            
+    
     
 def timestring_to_datetime(time_string) -> datetime:
     return dateutil.parser.parse(time_string)
